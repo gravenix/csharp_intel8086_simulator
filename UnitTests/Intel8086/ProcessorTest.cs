ce3af279a55225c09d87b37326e86808f7433716
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2020-05-12 22:41:24 +0200

    #REFACTOR: Register

--- UnitTests/Intel8086/ProcessorTest.cs
+++ UnitTests/Intel8086/ProcessorTest.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using symulator_rejestru.Intel8086;
 using symulator_rejestru.Intel8086.Instructions;
+using System;
 
 namespace UnitTests.Intel8086
 {
